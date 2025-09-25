d7bd77b58c3f682a7859c33952826cad26f766e2
MaquiMarinoni <118371660+MaquiMarinoni@users.noreply.github.com>
2025-09-24 21:26:30 -0300

    Avances 03.Introduccion a POO

--- 03 Introduccion a POO/01. Actividad 1/PrimerObjeto/src/primerobjeto/Auto.java
+++ 03 Introduccion a POO/01. Actividad 1/PrimerObjeto/src/primerobjeto/Auto.java
@@ -17,5 +17,15 @@ public class Auto {
     // si lo pongo asi por defecto todos los objetos van a tomar el valor "negro" en el atributo color al nacer
     int km;
     boolean conAire;
+    
+    // DEFINO UN METODO
+    // en particular, como este solo va  aimprimir por consola, delante del nombre del metodo ira
+    // 'void' que es un tipo de dato vacio
+    void mostrarDatos () {
+        System.out.println(patente + " " + color + " " + km + " " + conAire);
+    }
+    
+    // Una de las principale caracteristicas de los metodos es que son reutilizables
+    // es decir, buscan ordenar el codigo para que sea mas claro y legible
 
 }
--- 03 Introduccion a POO/01. Actividad 1/PrimerObjeto/src/primerobjeto/Principal.java
+++ 03 Introduccion a POO/01. Actividad 1/PrimerObjeto/src/primerobjeto/Principal.java
@@ -16,20 +16,16 @@ public class Principal {
     public static void main(String[] args) {
         Auto a = new Auto();
         // la linea que sigue imprime los datos por dafault - null, 0 y false
-        System.out.println(a.patente + " " + a.color + " " + a.km + " " + a.conAire);
+        // System.out.println(a.patente + " " + a.color + " " + a.km + " " + a.conAire);
+        // Puedo definir dentro de la class un metodo para que en todas las variables haga lo mismo
+        
+        // MOSTRAR DATOS SERA UN METODO
+        a.mostrarDatos();
         a.patente = "abc123";
         a.color = "negro";
         a.km = 500;
         a.conAire = true;
-        System.out.println(a.patente + " " + a.color + " " + a.km + " " + a.conAire);
-        
-        Auto a2 = new Auto();
-        System.out.println(a2.patente + " " + a2.color + " " + a2.km + " " + a2.conAire);
-        a2.patente = "def456";
-        a2.color = "blanco";
-        a2.km = 600;
-        a2.conAire = false;
-        System.out.println(a2.patente + " " + a2.color + " " + a2.km + " " + a2.conAire);
+        a.mostrarDatos();   
     }
     
 }
